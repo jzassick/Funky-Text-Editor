3be4a3867da9a16df15ee5507c55c34a1dc5ecbf
James Zassick <jzassick@gmail.com>
2023-02-15 15:16:40 -0500

    Frame class created to implement JFrame

---
+++ Frame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+	JFrame frame;
+
+	public JFrame getFrame(){
+		return this.frame;
+	}
+
+	public Frame(){
+		this.frame = new JFrame("editor");
+		this.getFrame().setVisible(true);
+		this.getFrame().setSize(250,250);
+	}
+
+}
--- launcher.java
+++ launcher.java
@@ -1,5 +1,5 @@
 public class launcher {
 	public static void main(String[] args){
-		System.out.println("main method");
+		Frame frame = new Frame();
 	}
 }
